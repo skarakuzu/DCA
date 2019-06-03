c13dcc91a4e0771b8f057050dcfdde3efeff823d
Peter Doak <doakpw@ornl.gov>
2019-06-03 15:32:05 -0400

    Following coding style for Types added test G4_sum
    it still fails

--- test/unit/phys/dca_step/cluster_solver/shared_tools/accumulation/tp/tp_accumulator_test.cpp
+++ test/unit/phys/dca_step/cluster_solver/shared_tools/accumulation/tp/tp_accumulator_test.cpp
@@ -92,3 +92,65 @@ TEST_F(TpAccumulatorTest, Accumulate) {
   else
     reader.close_file();
 }
+
+using TpAccumulatorG4SumTest =
+    dca::testing::G0Setup<dca::testing::LatticeBilayer, dca::phys::solver::CT_AUX, input_file>;
+
+TEST_F(TpAccumulatorG4SumTest, Accumulate) {
+  const std::array<int, 2> n{18, 22};
+  Sample M;
+  Configuration config;
+  ConfigGenerator::prepareConfiguration(config, M, TpAccumulatorTest::BDmn::dmn_size(),
+                                        TpAccumulatorTest::RDmn::dmn_size(), parameters_.get_beta(),
+                                        n);
+
+  Data::TpGreensFunctionSum G4_check_sum("G4_sum");
+  
+  dca::io::HDF5Writer writer;
+  dca::io::HDF5Reader reader;
+
+  const std::string baseline = INPUT_DIR "tp_accumulator_sum_test_baseline.hdf5";
+
+  if (update_baseline)
+    writer.open_file(baseline);
+  else
+    reader.open_file(baseline);
+
+  std::map<dca::phys::FourPointType, std::string> func_names;
+  func_names[dca::phys::PARTICLE_HOLE_TRANSVERSE] = "G4_ph_transverse";
+  func_names[dca::phys::PARTICLE_HOLE_MAGNETIC] = "G4_ph_magnetic";
+  func_names[dca::phys::PARTICLE_HOLE_CHARGE] = "G4_ph_charge";
+  func_names[dca::phys::PARTICLE_PARTICLE_UP_DOWN] = "G4_pp_up_down";
+
+  for (const dca::phys::FourPointType type :
+       {dca::phys::PARTICLE_HOLE_TRANSVERSE, dca::phys::PARTICLE_HOLE_MAGNETIC,
+        dca::phys::PARTICLE_HOLE_CHARGE, dca::phys::PARTICLE_PARTICLE_UP_DOWN}) {
+    parameters_.set_four_point_type(type);
+    parameters_.set_compute_all_frequency_transfers(true);
+
+
+    dca::phys::solver::accumulator::TpAccumulator<Parameters> accumulator(
+        data_->G0_k_w_cluster_excluded, parameters_);
+    
+    const int sign = 1;
+    accumulator.accumulate(M, config, sign);
+    accumulator.finalize();
+
+    const auto& G4 = accumulator.get_sign_times_G4();
+
+    if (update_baseline) {
+      writer.execute(func_names[type], G4);
+    }
+    else {
+      G4_check_sum.set_name(func_names[type]);
+      reader.execute(G4_check_sum);
+      const auto diff = dca::func::util::difference(G4, G4_check_sum);
+      EXPECT_GT(5e-7, diff.l_inf);
+    }
+  }
+
+  if (update_baseline)
+    writer.close_file();
+  else
+    reader.close_file();
+}
